fd08d48377a6af98b402b04383c453754e4c5318
gprerana212-oss <gprerana212@gmail.com>
2025-12-23 12:15:02 +0530

    pytest

---
+++ test_student_details.py
@@ -0,0 +1,14 @@
+from student import student_details
+
+def test_student_details():
+    sid = "001"
+    sname = "priya"
+    age = "20"
+
+    expected_output = (
+        "Student ID: 001\n"
+        "Student Name: priya\n"
+        "Age: 20\n"
+    )
+
+    assert student_details(sid, sname, age) == expected_output
